203dfe63cae07a41b2db3079009d5b6ab0f16020
rsansonpolker <rafasancon@hotmail.com>
2024-06-14 14:42:35 -0300

    Base platformer setup

---
+++ Core/Platform.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+from pygame.locals import *
+
+HEIGHT = 450
+WIDTH = 400
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((random.randint(50, 100), 12))
+        self.surf.fill((0, 255, 0))
+        self.rect = self.surf.get_rect(center=(random.randint(0, WIDTH - 10),
+                                               random.randint(0, HEIGHT - 30)))
+        self.speed = random.randint(-1, 1)
+
+        self.point = True
+        self.moving = True
+
+    def move(self):
+        if self.moving == True:
+            self.rect.move_ip(self.speed, 0)
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
---
+++ Core/Player.py
@@ -0,0 +1,66 @@
+import pygame
+from pygame.locals import *
+
+vec = pygame.math.Vector2
+
+FRIC = -0.12
+ACC = 0.5
+WIDTH = 400
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # self.image = pygame.image.load("character.png")
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((255, 255, 0))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((10, 360))
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.jumping = False
+        self.score = 0
+
+    def move(self):
+        self.acc = vec(0, 0.5)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        self.rect.midbottom = self.pos
+
+    def jump(self, platforms):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if hits and not self.jumping:
+            self.jumping = True
+            self.vel.y = -15
+
+    def cancel_jump(self):
+        if self.jumping:
+            if self.vel.y < -3:
+                self.vel.y = -3
+
+    def update(self, platforms):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if self.vel.y > 0:
+            if hits:
+                if self.pos.y < hits[0].rect.bottom:
+                    if hits[0].point == True:
+                        hits[0].point = False
+                        self.score += 1
+                    self.pos.y = hits[0].rect.top + 1
+                    self.vel.y = 0
+                    self.jumping = False
